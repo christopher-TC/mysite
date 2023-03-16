50512c87baa2fbc846a39d46e06d6faf60845566
witchwhichswitch <christopher.tchp@gmail.com>
2023-03-16 11:57:40 +0100

    ossidiseppiapush

---
+++ ossidiseppia/dist/precache-manifest.34304e03bb14e63f375b0f57881cf8e2.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = [
+  {
+    "revision": "a213f0c5eee824505243",
+    "url": "/css/app.b48c9906.css"
+  },
+  {
+    "revision": "a213f0c5eee824505243",
+    "url": "/js/app.95b3c189.js"
+  },
+  {
+    "revision": "6dedd74e65eddacb05a9",
+    "url": "/css/chunk-5964dff2.135f5148.css"
+  },
+  {
+    "revision": "6dedd74e65eddacb05a9",
+    "url": "/js/chunk-5964dff2.4e8245d5.js"
+  },
+  {
+    "revision": "4f51d2e320646baea902",
+    "url": "/css/chunk-63daebf2.5e66a57e.css"
+  },
+  {
+    "revision": "4f51d2e320646baea902",
+    "url": "/js/chunk-63daebf2.d48c2412.js"
+  },
+  {
+    "revision": "ef39e259de76dfa1c93f",
+    "url": "/css/chunk-7f9d0000.e4ae6cfc.css"
+  },
+  {
+    "revision": "ef39e259de76dfa1c93f",
+    "url": "/js/chunk-7f9d0000.a7d2fb73.js"
+  },
+  {
+    "revision": "3658df8ad5cbd2661028",
+    "url": "/css/chunk-9d8e7e54.74d6a2b4.css"
+  },
+  {
+    "revision": "3658df8ad5cbd2661028",
+    "url": "/js/chunk-9d8e7e54.966a156d.js"
+  },
+  {
+    "revision": "95ee02bb07d48a278925",
+    "url": "/css/chunk-vendors.6f5dfe48.css"
+  },
+  {
+    "revision": "95ee02bb07d48a278925",
+    "url": "/js/chunk-vendors.f1470275.js"
+  },
+  {
+    "revision": "6831db68fc7f202746573efc0f25a5d4",
+    "url": "/img/37_02_1493809115.6831db68.jpg"
+  },
+  {
+    "revision": "c53958c29c01cbb0be977eeb2df48ea6",
+    "url": "/img/internal.c53958c2.png"
+  },
+  {
+    "revision": "c183c5add1a38c3f7cc11a7de1540026",
+    "url": "/img/media-19.c183c5ad.jpg"
+  },
+  {
+    "revision": "a5645a6d787426b2be9a48f6472ed96d",
+    "url": "/img/logo.a5645a6d.png"
+  },
+  {
+    "revision": "f795aa2754b880cce07ac8bc048db136",
+    "url": "/img/media-20.f795aa27.jpg"
+  },
+  {
+    "revision": "92256efe239340cd16fc6debfefcd09a",
+    "url": "/img/media-2.92256efe.jpg"
+  },
+  {
+    "revision": "e82d6bfb5b05e1589144553f1237d81c",
+    "url": "/img/media-24.e82d6bfb.jpg"
+  },
+  {
+    "revision": "5be108239165acbe11fd501fc38b5b94",
+    "url": "/img/media-23.5be10823.jpg"
+  },
+  {
+    "revision": "c1692313f5051559d90f907d73f8346a",
+    "url": "/img/media-7.c1692313.jpg"
+  },
+  {
+    "revision": "2f8d6b94cec6e4e022f21b3247a02652",
+    "url": "/img/media-4.2f8d6b94.jpg"
+  },
+  {
+    "revision": "7055c08c781b281e34a1cc4e95786094",
+    "url": "/img/media-33.7055c08c.jpg"
+  },
+  {
+    "revision": "1e8e07172a5f14f5305734cb7c39ae1e",
+    "url": "/img/media-27.1e8e0717.jpg"
+  },
+  {
+    "revision": "c34538f8afcc9ab9d2331bcc4a4b890e",
+    "url": "/img/media-14.c34538f8.jpg"
+  },
+  {
+    "revision": "53b6d7396acc8e432e578c88a00efe10",
+    "url": "/img/media-22.53b6d739.jpg"
+  },
+  {
+    "revision": "8b8a3b886b01bed39175e5a725e22713",
+    "url": "/img/media-40.8b8a3b88.jpg"
+  },
+  {
+    "revision": "094349dd38ed61adb3377fb3a8ceda93",
+    "url": "/img/media-26.094349dd.jpg"
+  },
+  {
+    "revision": "25586f29664401167bb8cf00c712a2d0",
+    "url": "/img/media-1.25586f29.jpg"
+  },
+  {
+    "revision": "cd5d8db5ca01e2a3cb8ed7d7e81d44a1",
+    "url": "/img/media-32.cd5d8db5.jpg"
+  },
+  {
+    "revision": "87a3cf6a2518f78849e46ff18b222ef5",
+    "url": "/img/media-16.87a3cf6a.jpg"
+  },
+  {
+    "revision": "d440ed0dc6e875576513233bc18e5f63",
+    "url": "/img/media-10.d440ed0d.jpg"
+  },
+  {
+    "revision": "9e8f69a198d190d371aabc4566e1c5ea",
+    "url": "/img/media-3.9e8f69a1.jpg"
+  },
+  {
+    "revision": "4a3b1a71c47778edc1a7cba41633f77b",
+    "url": "/img/media-29.4a3b1a71.jpg"
+  },
+  {
+    "revision": "f17b00e0a29e62dea887292bd30055e4",
+    "url": "/img/media-9.f17b00e0.jpg"
+  },
+  {
+    "revision": "14e99041bebd58816cdec6acd64ca542",
+    "url": "/img/menu-degustazione.14e99041.png"
+  },
+  {
+    "revision": "a4236aed63aa6cc6b3dc1a4bf9e4c1d4",
+    "url": "/img/media-8.a4236aed.jpg"
+  },
+  {
+    "revision": "ed65397f2d41852fc62ce1cbeb71f327",
+    "url": "/img/media-39.ed65397f.jpg"
+  },
+  {
+    "revision": "2ef8baa9987ffc59438044e8dadc2c65",
+    "url": "/img/media-5.2ef8baa9.jpg"
+  },
+  {
+    "revision": "94fdbaa6e4412f65b687e5a5fd765a96",
+    "url": "/img/media-38.94fdbaa6.jpg"
+  },
+  {
+    "revision": "b068b301c7ed591fbf8f76d894767cef",
+    "url": "/img/media-37.b068b301.jpg"
+  },
+  {
+    "revision": "8f45588fc8cc0a552305c6f20e8eb351",
+    "url": "/img/media-18.8f45588f.jpg"
+  },
+  {
+    "revision": "34e7eb7d75280f5998a2f84f034c50a1",
+    "url": "/img/filosofia_title.34e7eb7d.png"
+  },
+  {
+    "revision": "66964c1d649541698372190043c2aa03",
+    "url": "/img/menu-carta.66964c1d.png"
+  },
+  {
+    "revision": "8b044536355de8e95bac9540e7d5f790",
+    "url": "/img/media-6.8b044536.jpg"
+  },
+  {
+    "revision": "7a4e64d5414f00157f3c275ec566b78b",
+    "url": "/img/media-31.7a4e64d5.jpg"
+  },
+  {
+    "revision": "60af728720ccf6693f4874cc6958690e",
+    "url": "/img/media-17.60af7287.jpg"
+  },
+  {
+    "revision": "db07e74cb193a44b7b4d7495013ee990",
+    "url": "/img/media-15.db07e74c.jpg"
+  },
+  {
+    "revision": "f8f001bc52a2dd262082fd95a79c0386",
+    "url": "/img/media-25.f8f001bc.jpg"
+  },
+  {
+    "revision": "713d1e4c089b35caf993e099ac5e7bff",
+    "url": "/img/media-21.713d1e4c.jpg"
+  },
+  {
+    "revision": "28fdd961f60892b7749f4822cd53fdea",
+    "url": "/img/media-11.28fdd961.jpg"
+  },
+  {
+    "revision": "e23c9625f3d861f98688984924488489",
+    "url": "/img/media-30.e23c9625.jpg"
+  },
+  {
+    "revision": "f2dd62199fd93bbd4a3c396cc9c655da",
+    "url": "/img/media-12.f2dd6219.jpg"
+  },
+  {
+    "revision": "e3c9346070b104ffd1cb453ab2fe5c06",
+    "url": "/img/filosofia-inner.e3c93460.png"
+  },
+  {
+    "revision": "c183c5add1a38c3f7cc11a7de1540026",
+    "url": "/img/media-34.c183c5ad.jpg"
+  },
+  {
+    "revision": "aac59f0da6f940d4bf8058edf5854308",
+    "url": "/img/media.aac59f0d.jpg"
+  },
+  {
+    "revision": "86c451a78a5dd307beec585b924d9933",
+    "url": "/img/media-13.86c451a7.jpg"
+  },
+  {
+    "revision": "12d625db6af90d3369fdd951236679c9",
+    "url": "/img/menu-vini.12d625db.png"
+  },
+  {
+    "revision": "f520953c6819625c4904b242e3d4134d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c53958c29c01cbb0be977eeb2df48ea6",
+    "url": "/img/desktop.png"
+  }
+];
